1cac6faa83056884fae49d9f1625e60aaa115790
svovko <simon.vovko@gmail.com>
2020-04-12 18:30:09 +0200

    Removed global keyword and added template_data as global variable.

--- observe.py
+++ observe.py
@@ -1,63 +1,82 @@
 from subprocess import call
 from flask import Flask, render_template
 from CTelescope import Telescope
-from MessierObjects import CelestialObject
+from sky_object_data import stars, messier_objects
+from co import CelestialObject
 import datetime
 
 
-t = Telescope(6, 16, 5, 13)
-
 app = Flask(__name__)
+
+t = Telescope(6, 16, 5, 13)
+template_data = {'title': 'My Scope', 'stars': stars, 'mo': messier_objects}
 
 
 @app.route('/')
 def index():
-    template_data = {'title': 'My Scope'}
+
     return render_template('index.html', **template_data)
 
 
 @app.route('/iso/<iso>')
 def set_iso(iso):
-    global t
+
     t.set_iso(int(iso))
-    template_data = {'title': 'My Scope', 'init': t.get_initialized()}
+    template_data['init'] = t.get_initialized()
     return render_template('index.html', **template_data)
 
 
-@app.route('/speed/<speed>')
-def set_speed(speed):
-    global t
-    t.set_speed(int(speed))
-    template_data = {'title': 'My Scope', 'init': t.get_initialized()}
+@app.route('/steps/<steps>')
+def set_steps(steps):
+
+    t.set_manual_steps(int(steps))
+    template_data['init'] = t.get_initialized()
     return render_template('index.html', **template_data)
 
 
 @app.route('/exit')
 def shutdown():
+
     call('sudo poweroff', shell=True)
 
 
 @app.route('/initialize/<dt>')
 def settime(dt):  # 45.801007399999996 15.1672683;2020-2-20 20:19:0
-    global t
+
     params = dt.split(';')
     t.set_location(params[0])
     call('sudo date -s "'+params[1]+'"', shell=True)
-    template_data = {'title': 'My Scope', 'init': t.get_initialized()}
+    template_data['init'] = t.get_initialized()
+    return render_template('index.html', **template_data)
+
+
+@app.route('/lookingAt/<param>')
+def looking_at(param):
+    # param ~ '72'
+
+    template_data['init'] = t.get_initialized()
+
+    p_obj_id = int(param)  # get object id
+
+    # get datetime data
+    dt = datetime.datetime.now()
+
+    mo = CelestialObject(p_obj_id)
+    template_data['Position'] = t.looking_at(mo, dt)
+
     return render_template('index.html', **template_data)
 
 
 @app.route('/locate/<param>')
 def locate(param):
     # param ~ '72'
-    global t
-    template_data = {'title': 'Locating object', 'init': t.get_initialized()}
+
+    template_data['init'] = t.get_initialized()
 
     p_obj_id = int(param)  # get object id
 
     # get datetime data
     dt = datetime.datetime.now()
-    # print(dt)
 
     mo = CelestialObject(p_obj_id)
     template_data['Position'] = t.locate(mo, dt)
@@ -67,8 +86,8 @@ def locate(param):
 
 @app.route('/dir/<dir>')
 def turn_scope(dir):
-    global t
-    template_data = {'title': 'My Scope', 'init': t.get_initialized()}
+
+    template_data['init'] = t.get_initialized()
 
     if dir == 'left':
         t.turn_left()
