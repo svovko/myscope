38676ae049a87cd18837f0c97d427463c865dfaa
svovko <simon.vovko@gmail.com>
2020-05-15 21:49:39 +0200

    Added camera settings. You can also start/stop the camera.

--- CCamera.py
+++ CCamera.py
@@ -2,6 +2,7 @@ import time
 import io
 import threading
 import picamera
+from fractions import Fraction
 
 
 class Camera:
@@ -17,6 +18,7 @@ class Camera:
 
     def start_streaming(self):
         self.camera = picamera.PiCamera()
+        self.camera.image_effect = 'colorswap'
         self.thread = threading.Thread(target=self.start_stream)
         self.thread.start()
 
@@ -26,11 +28,16 @@ class Camera:
     def get_picture(self):
         current = time.localtime()
         fname = '{}_{}_{}__{}_{}_{}.jpg'.format(current.tm_mday, current.tm_mon, current.tm_year,
-                                                                current.tm_hour, current.tm_min, current.tm_sec)
+                                                current.tm_hour, current.tm_min, current.tm_sec)
         self.camera.capture('static/pictures/'+fname, use_video_port=False)
         return fname
 
     def stop_streaming(self):
+        self.streaming = False
+        if self.thread is not None:
+            self.thread.join()
+
+    def quit(self):
         self.streaming = False
 
         print('Waiting for thread ...')
@@ -41,15 +48,26 @@ class Camera:
         if self.camera is not None:
             self.camera.close()
 
+    def set_iso(self, iso):
+        self.camera.iso = iso
+
+    def set_exp(self, exp):
+        self.camera.shutter_speed = exp * 1000000
+        self.camera.framerate = Fraction(1, exp)
+        self.camera.exposure_mode = 'off'
+
+    # possible TODO - if overheating - stop streaming
     def start_stream(self):
         self.streaming = True
         stream = io.BytesIO()
+
         for _ in self.camera.capture_continuous(stream, 'jpeg', use_video_port=True):
             stream.seek(0)
             self.frame = stream.read()
-
             stream.seek(0)
             stream.truncate()
+            # time.sleep(0.1)
 
             if not self.streaming:
+                self.camera.close()
                 break
