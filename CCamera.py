f036ec46b4310739154243b1a8c60401b3b06cac
svovko <simon.vovko@gmail.com>
2021-06-11 15:52:03 +0200

    Changed camera settings

--- CCamera.py
+++ CCamera.py
@@ -20,6 +20,11 @@ class Camera:
         self.camera = picamera.PiCamera()
         # self.camera.image_effect = 'colorswap'
         self.camera.rotation = 90
+        self.camera.iso = 100
+        self.camera.sensor_mode = 3
+        self.camera.resolution = (640, 480)
+        # self.camera.exposure_mode = 'night'
+
         self.thread = threading.Thread(target=self.start_stream)
         self.thread.start()
 
@@ -30,7 +35,17 @@ class Camera:
         current = time.localtime()
         fname = '{}_{}_{}__{}_{}_{}.jpg'.format(current.tm_mday, current.tm_mon, current.tm_year,
                                                 current.tm_hour, current.tm_min, current.tm_sec)
+
+        self.camera.resolution = (640, 480)
+        self.camera.shutter_speed = 5000000
+        self.camera.framerate = Fraction(1, 5)
+        time.sleep(3)
+        self.camera.exposure_mode = 'off'
         self.camera.capture('static/pictures/'+fname, use_video_port=False)
+
+        self.camera.resolution = (640, 480)
+        self.camera.shutter_speed = 0
+        self.camera.framerate = 30
         return fname
 
     #def stop_streaming(self):
@@ -50,18 +65,21 @@ class Camera:
             self.camera.close()
 
     def set_iso(self, iso):
+        #self.quit()
         self.camera.iso = iso
+        time.sleep(1)
+        #self.start_streaming()
 
-    def set_exp(self, exp):
-
-        if exp == 0:
-            self.camera.shutter_speed = 0
-            self.camera.framerate = 0
-        else:
-            self.camera.shutter_speed = exp * 1000000
-            self.camera.framerate = Fraction(1, exp)
-
-        self.camera.exposure_mode = 'off'
+    # def set_exp(self, exp):  # read the docs!!!!
+    #
+    #     if exp == 0:
+    #         self.camera.shutter_speed = 0
+    #         self.camera.framerate = 0
+    #     else:
+    #         self.camera.shutter_speed = exp * 1000000
+    #         self.camera.framerate = Fraction(1, exp)
+    #
+    #     self.camera.exposure_mode = 'off'
 
     def start_stream(self):
         self.streaming = True
