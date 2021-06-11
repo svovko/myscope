2c7f8c7f753c670e9a2136f8cfcbe91e4e8423a3
svovko <simon.vovko@gmail.com>
2021-06-11 15:50:57 +0200

    deleted emty lines

--- LocationInfo.py
+++ LocationInfo.py
@@ -1,28 +1,21 @@
 class LocationInfo:
 
-    def __init__(self, dd, dm, rh, rm, lad, lam, lod, lom, y, m, d, hour, minute, second):
-        self.dec_degrees = dd
-        self.dec_minutes = dm
+    # p = 45.801007399999996 15.1672683
+    def __init__(self):
+        #self.lat_degrees = 0
+        #self.lat_minutes = 0
+        #self.lon_degrees = 0
+        #self.lon_minutes = 0
+        self.latitude = 0
+        self.longitude = 0
 
-        self.ra_hours = rh
-        self.ra_minutes = rm
+    def set_location(self, p):
+        p = p.split(' ')
+        self.latitude = float(p[0])  # 45.801007399999996
+        self.longitude = float(p[1])  # 15.1672683
 
-        self.lat_degrees = lad
-        self.lat_minutes = lam
-        self.lon_degrees = lod
-        self.lon_minutes = lom
+        #self.lat_degrees = int(self.latitude)  # lat degrees
+        #self.lon_degrees = int(self.longitude)  # lon degrees
 
-        self.dtg_year = y
-        self.dtg_month = m
-        self.dtg_day = d
-        self.dtg_hour = hour
-        self.dtg_minute = minute
-        self.dtg_second = second
-
-    def printLI(self):
-        print('RA:', self.ra_hours, self.ra_minutes)
-        print('DEC:', self.dec_degrees, self.dec_minutes)
-        print('LATITUDE:', self.lat_degrees, self.lat_minutes)
-        print('LONGITUDE:', self.lon_degrees, self.lon_minutes)
-        print('DATE:', self.dtg_year, self.dtg_month, self.dtg_day)
-        print('TIME:', self.dtg_hour, self.dtg_minute, self.dtg_second)
+        #self.lat_minutes = round((self.latitude - self.lat_degrees) * 60)  # lat minutes
+        #self.lon_minutes = round((self.longitude - self.lon_degrees) * 60)  # lon minutes
