6bf4f8373f1a404ef85c6d4f4bd38b792b980316
svovko <simon.vovko@gmail.com>
2022-01-15 06:40:43 +0100

    Added routes for mousedown and keydown support

--- observe.py
+++ observe.py
@@ -1,21 +1,30 @@
 import threading
 from subprocess import call
 from flask import Flask, render_template, jsonify, Response, request
+from flask_socketio import SocketIO
 
 from CTelescope import Telescope
-from CCamera import Camera
+# from CCamera import Camera
 
 from sky_object_data import get_objects, find_objects
 from LocationInfo import LocationInfo
 from TargetInfo import TargetInfo
 
 
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
 
 t = Telescope(6, 13, 5, 19)
-c = Camera()
+# c = Camera()
 li = LocationInfo()
 ti = TargetInfo()  # last target (for tracking)
+
+@socketio.on('message')
+def handle_message(data):
+    print('received message: ' + data)
 
 
 @app.route('/')
@@ -61,8 +70,8 @@ def set_time_and_location(dt):  # 45.801007399999996 15.1672683;2020-2-20 20:19:
     call('sudo date -s "'+params[1]+'"', shell=True)
 
     # start camera
-    if not c.streaming:
-        c.start_streaming()
+    # if not c.streaming:
+    #     c.start_streaming()
 
     return jsonify({'message': 'Location & time set: ' + dt})
 
@@ -140,6 +149,36 @@ def turn_scope(direction):
         "message": 'Turned ' + direction,
     })
 
+@app.route('/start_turning/<dir>')
+def start_turning(dir):
+    if dir == 'left':
+        t.start_turning_left()
+    elif dir == 'right':
+        t.start_turning_right()
+    elif dir == 'up':
+        t.start_turning_up()
+    elif dir == 'down':
+        t.start_turning_down()
+
+    return jsonify({
+        "message": 'Startet turning ' + dir,
+    })
+    
+
+@app.route('/stop_turning/<dir>')
+def stop_turning(dir):
+    if dir == 'left':
+        t.stop_turning_left()
+    elif dir == 'right':
+        t.stop_turning_right()
+    elif dir == 'up':
+        t.stop_turning_up()
+    elif dir == 'down':
+        t.stop_turning_down()
+
+    return jsonify({
+        "message": 'Stoped turning ' + dir,
+    })
 
 # reset position
 @app.route('/resetPosition')
@@ -150,44 +189,44 @@ def reset_position():
     })
 
 
-def gen():
+# def gen():
 
-    while True:
-        frame = c.get_frame()
-        if frame is not None:
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-
-@app.route('/take_picture')
-def take_picture():  # tale zna crknit, če je kamera ugasnjena
-
-    img = c.get_picture()
-    #print('Took image:', img)
-    return jsonify({
-        "message": "Image saved to: <a href='static/pictures/"+img+"' target='_blank'>"+img+"</a>",
-        "src": "static/pictures/"+img,
-    })
+#     while True:
+#         frame = c.get_frame()
+#         if frame is not None:
+#             yield (b'--frame\r\n'
+#                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
-@app.route('/video_feed')
-def video_feed():
+# @app.route('/take_picture')
+# def take_picture():  # tale zna crknit, če je kamera ugasnjena
 
-    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+#     img = c.get_picture()
+#     #print('Took image:', img)
+#     return jsonify({
+#         "message": "Image saved to: <a href='static/pictures/"+img+"' target='_blank'>"+img+"</a>",
+#         "src": "static/pictures/"+img,
+#     })
+
+
+# @app.route('/video_feed')
+# def video_feed():
+
+#     return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 @app.route('/exit')
 def shutdown():
 
-    c.quit()
+    # c.quit() # quit camera
     call('sudo poweroff', shell=True)
 
 
-@app.route('/set_iso/<iso>')
-def set_iso(iso):
+# @app.route('/set_iso/<iso>')
+# def set_iso(iso):
 
-    c.set_iso(int(iso))
-    return jsonify({"message": "ISO set to: " + iso, })
+#     c.set_iso(int(iso))
+#     return jsonify({"message": "ISO set to: " + iso, })
 
 
 # @app.route('/set_exp/<exp>')
@@ -198,4 +237,5 @@ def set_iso(iso):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False, ssl_context='adhoc')
+    # app.run(host='0.0.0.0', debug=False, port=80, ssl_context=('telescope.crt', 'telescope.key'))
+    socketio.run(app=app, host='0.0.0.0', debug=False, port=80, ssl_context=('telescope.crt', 'telescope.key'))
