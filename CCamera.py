5155c00050787e538e0444f1a280e84a3c90db9c
svovko <simon.vovko@gmail.com>
2022-03-01 06:34:46 +0100

    disabled camera

--- CCamera.py
+++ CCamera.py
@@ -14,38 +14,49 @@ class Camera:
 
         self.streaming = False
 
-        # TODO - camera settings
+        # camera settings
+        self.c_rotation = 90
+        self.c_iso = 100
+        self.c_sensor_mode = 3
+        self.c_resolution = (1280, 720)
+        self.c_exposure_mode = 'off' # možnost je še: 'night'
+        #self.c_framerate = Fraction(1, 6)
 
     def start_streaming(self):
-        self.camera = picamera.PiCamera()
-        # self.camera.image_effect = 'colorswap'
-        self.camera.rotation = 90
-        self.camera.iso = 100
-        self.camera.sensor_mode = 3
-        self.camera.resolution = (640, 480)
-        # self.camera.exposure_mode = 'night'
+        print('Opening camera ...')
+        self.camera             = picamera.PiCamera()
+        self.camera.rotation    = self.c_rotation
+        self.camera.iso         = self.c_iso # pravijo, da bi dal tole na 800 for maximum gain
+        #self.camera.sensor_mode = self.c_sensor_mode
+        self.camera.resolution  = self.c_resolution
+        # probejmo tole spodaj:
+        #self.camera.shutter_speed = 6000000
+        #self.camera.framerate = self.c_framerate
+        #self.camera.exposure_mode = self.c_exposure_mode
+        time.sleep(5)
 
         self.thread = threading.Thread(target=self.start_stream)
         self.thread.start()
 
+
     def get_frame(self):
         return self.frame
+
 
     def get_picture(self):
         current = time.localtime()
         fname = '{}_{}_{}__{}_{}_{}.jpg'.format(current.tm_mday, current.tm_mon, current.tm_year,
                                                 current.tm_hour, current.tm_min, current.tm_sec)
 
-        self.camera.resolution = (640, 480)
-        self.camera.shutter_speed = 5000000
-        self.camera.framerate = Fraction(1, 5)
-        time.sleep(3)
-        self.camera.exposure_mode = 'off'
+        # self.camera.resolution = (640, 480)
+        # self.camera.shutter_speed = 6000000
+        # self.camera.framerate = Fraction(1, 6)
+        # time.sleep(3)
+        # self.camera.exposure_mode = 'off'
         self.camera.capture('static/pictures/'+fname, use_video_port=False)
 
-        self.camera.resolution = (640, 480)
-        self.camera.shutter_speed = 0
-        self.camera.framerate = 30
+        #self.camera.shutter_speed = 0
+        #self.camera.framerate = 30 # mogoče zaradi tega potem slike tako svetle???
         return fname
 
     #def stop_streaming(self):
@@ -63,12 +74,13 @@ class Camera:
         print('Closing camera ... ')
         if self.camera is not None:
             self.camera.close()
+            print('Camera closed!')
 
     def set_iso(self, iso):
-        #self.quit()
-        self.camera.iso = iso
+        self.quit()
+        self.c_iso = iso
         time.sleep(1)
-        #self.start_streaming()
+        self.start_streaming()
 
     # def set_exp(self, exp):  # read the docs!!!!
     #
